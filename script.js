bd8796805a77cc42df8a9df49e78d551ff08f1b6
Miken <m0122333@gmail.com>
2020-05-18 10:50:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log (leftBorderWidth)
+
+let arr = ['plump','orange','apple'];
+console.log(arr[2]);
+
+//alert('Hello World');
+
+//let answer = confirm('are you shure?');
+//console.log(answer);
+
+let answer = prompt("есть ли вам 18 лет?", "Да");
+console.log(typeof(answer));
